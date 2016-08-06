dad878258b2769b20ec479d26c9f4e6d6499522a
Chris <cxholland@gmail.com>
2016-08-05 19:49:22 -0600

    a

---
+++ CH_Initials_LOGO_PRINT.py
@@ -0,0 +1,57 @@
+import time
+
+
+time.sleep(0.1)
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print" CCCCCCCCCCCCC   XXX            XXX   HHH      HHH "
+time.sleep(0.1)
+print" CCCCCCCCCCCCC    XXX          XXX    HHH      HHH "
+time.sleep(0.1)
+print" CCCC               XXX      XXX      HHH      HHH "
+time.sleep(0.1)
+print" CCCC                XXXXXXXXXX       HHHHHHHHHHHH "
+time.sleep(0.1)
+print" CCCC                XXXXXXXXXX       HHHHHHHHHHHH "
+time.sleep(0.1)
+print" CCCC               XXX      XXX      HHH      HHH "
+time.sleep(0.1)
+print" CCCCCCCCCCCCC    XXX          XXX    HHH      HHH "
+time.sleep(0.1)
+print" CCCCCCCCCCCCC   XXX            XXX   HHH      HHH "
+time.sleep(0.25)
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+print"   "
+time.sleep(0.5)
